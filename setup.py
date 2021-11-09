3449dd24c578e7078b29d5c4866668bab81b7350
sybrenjansen <sybren.jansen@gmail.com>
2021-11-09 11:44:15 +0100

    Pinned docutils to older 0.17.1 version for the documentation

--- setup.py
+++ setup.py
@@ -31,7 +31,8 @@ if __name__ == "__main__":
         extras_require={
             "dashboard": ["flask"],
             "dill": ["multiprocess"],
-            "docs": ["sphinx==3.2.1",
+            "docs": ["docutils==0.17.1"
+                     "sphinx==3.2.1",
                      "sphinx-rtd-theme==0.5.0",
                      "sphinx-autodoc-typehints==1.11.0",
                      "sphinxcontrib-images==0.9.2",
