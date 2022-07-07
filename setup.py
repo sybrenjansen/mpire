9fae6e84b4f7d564aef5a72c60cf3e5214058070
ddelange <14880945+ddelange@users.noreply.github.com>
2022-07-07 09:14:20 +0200

    Add lower pin to tqdm
    ```console
    $ pip install -q 'tqdm<4.27' && python -c "from tqdm.auto import tqdm"
    Traceback (most recent call last):
      File "<string>", line 1, in <module>
    ModuleNotFoundError: No module named 'tqdm.auto'
    ```

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         scripts=['bin/mpire-dashboard'],
         install_requires=['dataclasses; python_version<"3.7"',
                           'pywin32==225; platform_system=="Windows"',
-                          'tqdm'],
+                          'tqdm>=4.27'],
         include_package_data=True,
         extras_require={
             'dashboard': ['flask'],
