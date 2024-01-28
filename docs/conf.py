a1471ae53369b861d3d79fed72c780dfaccd9dd6
sybrenjansen <sybrenjansen@gmail.com>
2024-01-28 20:17:36 +0100

    sphinx docs config updates

--- docs/conf.py
+++ docs/conf.py
@@ -208,6 +208,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.mathjax',
     'sphinx_autodoc_typehints',
+    'sphinx_copybutton',
     'sphinxcontrib.images'
 ]
 
@@ -247,7 +248,7 @@ release = _version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
